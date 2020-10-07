9f26b7f0cac61c077bf9cab116c0cd787b3eeddb
Muhammad Iqbal <71365593+sirdar9009@users.noreply.github.com>
2020-10-07 14:59:29 +0700

    Add files via upload

---
+++ tugas4b.php
@@ -0,0 +1,8 @@
+<?php
+$j_saluran=523;
+$j_terpakai=8891;
+echo "Jumlah Saluran = ",$j_saluran, "<br/>";
+echo "Jumlah Terpakai = ",$j_terpakai, "<br/>";
+echo "Rata-rata Pemakaian = ......? <br/>";
+echo "Jawaban = ",$Jawab=$j_terpakai/$j_saluran, " Liter/Keluarga";
+?>
